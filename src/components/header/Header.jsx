6bbb3ac73b988a2756b34fef5ef1e850adf512c3
Berdimyrat24 <hanmamedowsaparmyrat@gmail.com>
2024-09-02 16:57:51 +0500

    Header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,43 @@
+import "./Header.css";
+
+import header_logo from "./header-svg/Frame.svg";
+import menu from "./header-img/menu.png";
+import { useState } from "react";
+
+const Header = () => {
+  const [link, setLink] = useState(false);
+
+  const clickMenu = () => {
+    setLink((currentValue) => {
+      return currentValue === "header_right" ? "active" : "header_right";
+    });
+  };
+  return (
+    <>
+      <header>
+        <div className="wrapper flex item-center">
+          <div className="header_left">
+            <img src={header_logo} alt="header_logo" />
+            <h1>Learning-center</h1>r
+          </div>
+          <div
+            className={
+              link ? ["header_right", "active"].join(" ") : ["header_right"]
+            }
+          >
+            <a href="#">Books</a>
+            <a href="#">Courses</a>
+            <a href="#">Others</a>
+            <a href="#">Blog</a>
+            <a href="#">Sing in</a>
+          </div>
+          <button onClick={clickMenu} className="menu">
+            <img src={menu} alt="menu" />
+          </button>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
