660fb2c51006bcdbe05706799ca18438258d8284
Berdimyrat24 <hanmamedowsaparmyrat@gmail.com>
2024-09-05 14:48:29 +0500

    footer-end

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,29 @@
 import "./Footer.css";
+import logo from "..//header/header-svg/Frame.svg";
 const Footer = () => {
   return (
     <>
       <footer className="footer-background">
         <div className="lt-wrapper_noCenter">
-          <div className="flex item-center">
-            <div className="left-block_footer"></div>
-            <div className="right-block_footer"></div>
+          <div className="flex item-center col-gap4rem ">
+            <div className="left-block_footer">
+              <img src={logo} alt="" />
+              <p>Okuw merkezi</p>
+            </div>
+            <div className="right-block_footer">
+              <details>
+                <summary>Email</summary>
+                <em>hanmamedow05gmail.com</em>
+              </details>
+              <details>
+                <summary>Telefon</summary>
+                <em>(+993) xx-xx-xx</em>
+              </details>
+              <details>
+                <summary>Salgymyz</summary>
+                <em>Turkmenabat şäheri</em>
+              </details>
+            </div>
           </div>
         </div>
       </footer>
